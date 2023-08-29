ca3bad7878ab4c00e26c71a93890468ab1bcbfd8
AliaksaPlh <sashvero@gmail.com>
2023-08-29 16:33:41 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let letterInBinary;
+    let letterInMorse;
+    let decodedLetters = '';
+    for(let i = 0; i < expr.length; i += 10) {
+        if (expr[i] === '*') {
+            decodedLetters = decodedLetters + ' ';
+        } else {
+            letterInBinary = `${+expr.slice(i, i + 10)}`;
+            letterInMorse = '';
+            for (let iPair = 0; iPair < letterInBinary.length; iPair += 2) {
+                if (letterInBinary.slice(iPair, iPair + 2) === '11') {
+                    letterInMorse = letterInMorse + '-';
+                } else if (letterInBinary.slice(iPair, iPair + 2) === '10') {
+                    letterInMorse = letterInMorse + '.';
+                }
+            }
+            decodedLetters = decodedLetters + MORSE_TABLE[letterInMorse];
+        }
+    }
+    return decodedLetters;
 }
 
 module.exports = {
